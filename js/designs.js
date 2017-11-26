d7923dea7481be01c2bfff2993d06620b583f8b5
Aleksander Fret <alemik@op.pl>
2017-11-27 00:52:07 +0100

    styling art pixel

--- js/designs.js
+++ js/designs.js
@@ -1,12 +1,17 @@
 // iife for encapsulation
 (function pixelArtMaker() {
-  const colorPicker = $('#colorPicker');
+  // Gets DOM elements
   const pixelCanvas = $('#pixel_canvas');
+  const colorPicker = $('#color_picker');
+  const backgroundColorPicker = $('#background_color_picker');
+  const borderColorPicker = $('#border_color_picker');
 
-  // Sets initial color value
+  // Sets initial value
   let color = colorPicker.val();
+  let backgroundColor = backgroundColorPicker.val();
+  let borderColor = borderColorPicker.val();
 
-  // Sets color value on colorPicker change (to avoid checking color in every paintHandler call).
+  // Sets color value on colorPicker change
   colorPicker.on('change', function colorChangeHandler() {
     color = $(this).val();
   });
@@ -27,16 +32,11 @@
       grid += '</tr>';
     }
     pixelCanvas.html(grid);
-    pixelCanvas.find('td').addClass('borderedCells');
+    pixelCanvas.find('td').addClass('bordered_cells').css({
+      'border-color': borderColor,
+      'background-color': backgroundColor
+    });
   }
-
-  $('#borderSwitcher').on('click', function cellBorderSwitchHandler() {
-    pixelCanvas.find('td').toggleClass('borderedCells');
-});
-
-  $('#backgroundCleaner').on('click', function cellBackgroundCleanHandler() {
-    pixelCanvas.find('td').css('background-color', '#fff');
-  });
 
   /**
    * @description Changes color of the grid cell
@@ -46,6 +46,56 @@
     event.preventDefault();
     $(event.target).css('background-color', color);
   }
+
+  /**
+   * @description Converts rgb color to hex color
+   * @param {string} rgb - rgb color
+   * @return {string}
+   */
+  function rgb2hex(rgb) {
+    rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+    return (rgb && rgb.length === 4) ? "#" +
+      ("0" + parseInt(rgb[1], 10).toString(16)).slice(-2) +
+      ("0" + parseInt(rgb[2], 10).toString(16)).slice(-2) +
+      ("0" + parseInt(rgb[3], 10).toString(16)).slice(-2) : '';
+  }
+
+  $('#border_switcher').on('click', function cellBorderSwitchHandler() {
+    pixelCanvas.find('td').toggleClass('bordered_cells');
+});
+
+  borderColorPicker.on('change', function borderColorChangeHandler() {
+    borderColor = $(this).val();
+    pixelCanvas.find('td').css('border-color', borderColor);
+  });
+
+  backgroundColorPicker.on('change', function backgroundVolorChangeHandler() {
+    const oldBackgroundColor = backgroundColor;
+    backgroundColor = $(this).val();
+    pixelCanvas.find('td').each(function filterCellsWithOldBackground(index, el) {
+      if (rgb2hex($(el).css('background-color')) === oldBackgroundColor) {
+        $(el).css('background-color', backgroundColor);
+      }
+    });
+  });
+
+  $('#background_cleaner').on('click', function cellBackgroundCleanHandler() {
+    pixelCanvas.find('td').css('background-color', backgroundColor);
+  });
+
+  $('#reset_painter').on('click', function resetPainter() {
+    backgroundColor = '#ffffff';
+    color = '#000000';
+    borderColor = '#000000';
+    colorPicker[0].value = color;
+    backgroundColorPicker[0].value = backgroundColor;
+    borderColorPicker[0].value = borderColor;
+    pixelCanvas.find('td').css({
+      'background-color': backgroundColor,
+      'color': color,
+      'border-color': borderColor
+    });
+  });
 
   // Ensures that height and width will match the min and max attributes
   $('#input_width, #input_height').on('blur', function dimensionValidatorHandler() {
@@ -60,7 +110,7 @@
     }
   });
 
-  // When grid size is submitted, creates the grid based on entered values
+  // When grid size is submitted, creates the grid based on entered values by calling makeGrid()
   $('.submit').on('click', function setDimensionsHandler(event) {
     event.preventDefault();
     const height = $('#input_height').val();
