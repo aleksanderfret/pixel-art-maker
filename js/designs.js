27673cae07b52a51e441276a43be2c770a3a94dc
Aleksander Fret <alemik@op.pl>
2017-11-26 16:07:37 +0100

    Functionality ready.

--- js/designs.js
+++ js/designs.js
@@ -1,10 +1,86 @@
-// Select color input
-// Select size input
+// iife for encapsulation
+(function pixelArtMaker() {
+  const colorPicker = $('#colorPicker');
+  const pixelCanvas = $('#pixel_canvas');
 
-// When size is submitted by the user, call makeGrid()
+  // Sets initial color value
+  let color = colorPicker.val();
 
-function makeGrid() {
+  // Sets color value on colorPicker change (to avoid checking color in every paintHandler call).
+  colorPicker.on('change', function colorChangeHandler() {
+    color = $(this).val();
+  });
 
-// Your code goes here!
+  /**
+   * @description Creates grid
+   * @param {number} height - number of rows
+   * @param {number} width - number of columns
+   */
+  function makeGrid(height, width) {
+    let grid = '';
 
-}
+    for (let row = 1; row <= height; row++) {
+      grid += '<tr>';
+      for (let col = 1; col <= width; col++) {
+        grid += '<td></td>';
+      }
+      grid += '</tr>';
+    }
+    pixelCanvas.html(grid);
+    pixelCanvas.find('td').addClass('borderedCells');
+  }
+
+  $('#borderSwitcher').on('click', function cellBorderSwitchHandler() {
+    pixelCanvas.find('td').toggleClass('borderedCells');
+});
+
+  $('#backgroundCleaner').on('click', function cellBackgroundCleanHandler() {
+    pixelCanvas.find('td').css('background-color', '#fff');
+  });
+
+  /**
+   * @description Changes color of the grid cell
+   * @param {object} event
+   */
+  function paintHandler(event) {
+    event.preventDefault();
+    $(event.target).css('background-color', color);
+  }
+
+  // Ensures that height and width will match the min and max attributes
+  $('#input_width, #input_height').on('blur', function dimensionValidatorHandler() {
+    const min = Number($(this).attr('min'));
+    const max = Number($(this).attr('max'));
+
+    if (Number($(this).val()) < min) {
+      $(this).val(min);
+    }
+    if (Number($(this).val()) > max) {
+      $(this).val(max);
+    }
+  });
+
+  // When grid size is submitted, creates the grid based on entered values
+  $('.submit').on('click', function setDimensionsHandler(event) {
+    event.preventDefault();
+    const height = $('#input_height').val();
+    const width = $('#input_width').val();
+
+    makeGrid(height, width);
+  });
+
+  // Starts painting
+  pixelCanvas.on('mousedown', 'td', function startPaintingHandler(event) {
+    event.preventDefault();
+
+    // Paints current cell and additionally handles painting on mouseover event
+    paintHandler(event);
+    pixelCanvas.on('mouseover', 'td', paintHandler);
+
+    // Stops painting on mouseup event
+    $('body').one('mouseup', function() {
+      event.preventDefault();
+      pixelCanvas.off('mouseover', 'td', paintHandler);
+    });
+  });
+})();
