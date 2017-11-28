288b386f4ed956c8a43ba3ecdc966661dcca3fa7
Aleksander Fret <alemik@op.pl>
2017-11-28 21:46:42 +0100

    functionality next code improvements

--- js/designs.js
+++ js/designs.js
@@ -6,18 +6,19 @@
   const backgroundColorPicker = $('#background_color_picker');
   const borderColorPicker = $('#border_color_picker');
 
-  // Sets initial value
-  let color = colorPicker.val();
-  let backgroundColor = backgroundColorPicker.val();
-  let borderColor = borderColorPicker.val();
+  // Captures initial values from DOM elements
+  const initialBrushColor = colorPicker.val();
+  const initialBackgroundColor = backgroundColorPicker.val();
+  const initialBorderColor = borderColorPicker.val();
+
+  // Sets current values based on initial values
+  let brushColor = initialBrushColor;
+  let backgroundColor = initialBackgroundColor;
+  let borderColor = initialBorderColor;
   let displayBorders = true;
-  // Sets color value on colorPicker change
-  colorPicker.on('change', function colorChangeHandler() {
-    color = $(this).val();
-  });
 
   /**
-   * @description Draw grid
+   * @description Draws grid
    * @param {number} height - number of rows
    * @param {number} width - number of columns
    */
@@ -39,76 +40,91 @@
   }
 
   /**
-   * @description Changes color of the grid cell
-   * @param {object} event
-   */
-  function paintHandler(event) {
-    event.preventDefault();
-    $(event.target).css('background-color', color);
-  }
-
-  /**
    * @description Converts rgb color to hex color
    * @param {string} rgb - rgb color
    * @return {string}
    */
   function rgb2hex(rgb) {
-    rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
-    return (rgb && rgb.length === 4) ? "#" +
-      ("0" + parseInt(rgb[1], 10).toString(16)).slice(-2) +
-      ("0" + parseInt(rgb[2], 10).toString(16)).slice(-2) +
-      ("0" + parseInt(rgb[3], 10).toString(16)).slice(-2) : '';
+    const rgbValues = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+    return (rgb && rgbValues.length === 4) ? '#' +
+      ('0' + parseInt(rgbValues[1], 10).toString(16)).slice(-2) +
+      ('0' + parseInt(rgbValues[2], 10).toString(16)).slice(-2) +
+      ('0' + parseInt(rgbValues[3], 10).toString(16)).slice(-2) : '';
   }
 
+  /**
+   * @description Changes color of cells from one to another
+   * @param {string} oldColor
+   * @param {string} newColor
+   */
+  function cellBackgroundChanger(oldColor, newColor) {
+    pixelCanvas.find('td').each(function filterCellsWithOldBackground(index, el) {
+      if (rgb2hex($(el).css('background-color')) === oldColor) {
+        $(el).css('background-color', newColor);
+      }
+    });
+  }
+
+  /**
+   * @description Changes color of the grid cell
+   * @param {object} event
+   */
+  function paintHandler(event) {
+    event.preventDefault();
+    $(event.target).css('background-color', brushColor);
+  }
+
+  // ----- EVENTS HANDLING -----
+  // Sets brush color value on colorPicker change
+  colorPicker.on('change', function colorChangeHandler() {
+    brushColor = $(this).val();
+  });
+
+  // Hides or shows cell borders
   $('#border_switcher').on('click', function cellBorderSwitchHandler() {
     pixelCanvas.find('td').toggleClass('bordered_cells');
-    $(this).attr('value', (displayBorders) ? 'Show:' : 'Hide:');
+    $(this).attr('value', (displayBorders) ? 'Show' : 'Hide');
     displayBorders = !displayBorders;
   });
 
+  // Sets border color on borderColorPicker change
   borderColorPicker.on('change', function borderColorChangeHandler() {
     borderColor = $(this).val();
     pixelCanvas.find('td').css('border-color', borderColor);
   });
 
-  backgroundColorPicker.on('change', function backgroundVolorChangeHandler() {
-    const oldBackgroundColor = backgroundColor;
+  // Changes cell background color on backgroundColorPicker change
+  backgroundColorPicker.on('change', function backgroundColorChangeHandler() {
+    cellBackgroundChanger(backgroundColor, $(this).val());
     backgroundColor = $(this).val();
-    pixelCanvas.find('td').each(function filterCellsWithOldBackground(index, el) {
-      if (rgb2hex($(el).css('background-color')) === oldBackgroundColor) {
-        $(el).css('background-color', backgroundColor);
-      }
-    });
   });
 
+  // Clean background color on button click
   $('#background_cleaner').on('click', function cellBackgroundCleanHandler() {
+    cellBackgroundChanger(backgroundColor, initialBackgroundColor);
+    backgroundColor = initialBackgroundColor;
     backgroundColorPicker[0].value = backgroundColor;
-    const oldBackgroundColor = backgroundColor;
-    backgroundColor = '#ffffff';
-    pixelCanvas.find('td').each(function filterCellsWithOldBackground(index, el) {
-      if (rgb2hex($(el).css('background-color')) === oldBackgroundColor) {
-        $(el).css('background-color', backgroundColor);
-      }
-    });
   });
 
-  $('#brush_cleaner').on('click', function cellBackgroundCleanHandler() {
+  // Clears paintig
+  $('#brush_cleaner').on('click', function cellBrushCleanHandler() {
     pixelCanvas.find('td').css('background-color', backgroundColor);
   });
 
+  // Resets canvas to initial state except of grid size
   $('#clear_canvas').on('click', function resetPainter() {
-    backgroundColor = '#ffffff';
-    color = '#000000';
-    borderColor = '#000000';
-    colorPicker[0].value = color;
+    backgroundColor = initialBackgroundColor;
+    brushColor = initialBrushColor;
+    borderColor = initialBorderColor;
+    displayBorders = true;
+
+    colorPicker[0].value = brushColor;
     backgroundColorPicker[0].value = backgroundColor;
     borderColorPicker[0].value = borderColor;
-    displayBorders = true;
-    pixelCanvas.find('td').toggleClass('bordered_cells');
-    $('#border_switcher').attr('value', 'Hide:');
+    pixelCanvas.find('td').addClass('bordered_cells');
+    $('#border_switcher').attr('value', 'Hide');
     pixelCanvas.find('td').css({
       'background-color': backgroundColor,
-      'color': color,
       'border-color': borderColor
     });
   });
@@ -126,7 +142,7 @@
     }
   });
 
-  // When grid size is submitted, creates the grid based on entered values by calling makeGrid()
+  // Creates the grid based on entered values, when grid size is submitted
   $('.submit').on('click', function setDimensionsHandler(event) {
     event.preventDefault();
     const height = $('#input_height').val();
@@ -139,12 +155,12 @@
   pixelCanvas.on('mousedown', 'td', function startPaintingHandler(event) {
     event.preventDefault();
 
-    // Paints current cell and additionally handles painting on mouseover event
+    // Paints current cell and additionally starts handling painting on mouseover event
     paintHandler(event);
     pixelCanvas.on('mouseover', 'td', paintHandler);
 
     // Stops painting on mouseup event
-    $('body').one('mouseup', function() {
+    $('body').one('mouseup', function stopPaintingHandler() {
       event.preventDefault();
       pixelCanvas.off('mouseover', 'td', paintHandler);
     });
